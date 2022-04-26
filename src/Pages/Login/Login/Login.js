ae1dea98799d4d9bfafad9fa563e250766802be5
zubayer-salehin <zubaersalehin@gmail.com>
2022-04-26 21:59:39 +0600

    hello

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,7 +8,6 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
-import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -32,22 +31,19 @@ const Login = () => {
     }
 
     if (user) {
-        // navigate(from, { replace: true });
+        navigate(from, { replace: true });
     }
 
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = async event => {
+    const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        await signInWithEmailAndPassword(email, password);
-        const { data } = await axios.post("http://localhost:5000/login", { email })
-        localStorage.setItem("accessToken",data.accessToken)
-        navigate(from, { replace: true });
+        signInWithEmailAndPassword(email, password);
     }
 
     const navigateRegister = event => {
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,35 +1,24 @@
 import axios from 'axios';
-import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Orders = () => {
 
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const email = user.email;
-        try{
-            axios.get(`http://localhost:5000/orders?email=${email}`, {
-            headers: {
-                authorization: `Bearer ${localStorage.getItem("accessToken")}`
-            }
-        })
-            .then(res => {
-                const { data } = res;
-                setOrders(data);
-            })
+        try {
+            axios.get(`http://localhost:5000/orders?email=${email}`)
+                .then(res => {
+                    const { data } = res;
+                    setOrders(data);
+                })
         }
-        catch(error){
+        catch (error) {
             console.log(error);
-            if (error.response.status === 401 || error.response.status === 403) {
-                signOut(auth);
-                navigate("/login");
-            }
         }
     }, [user])
 
